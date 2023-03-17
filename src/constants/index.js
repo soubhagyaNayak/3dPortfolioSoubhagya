fd9f9baa600d9c3cb8cade7d05e328254f1cae52
soubhagya <soubhagyaranjannayak68@gmail.com>
2023-03-17 09:32:28 +0530

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,236 @@
+import {
+    mobile,
+    backend,
+    creator,
+    web,
+    javascript,
+    typescript,
+    html,
+    css,
+    reactjs,
+    redux,
+    tailwind,
+    nodejs,
+    mongodb,
+    git,
+    figma,
+    docker,
+    meta,
+    starbucks,
+    tesla,
+    shopify,
+    carrent,
+    jobit,
+    tripguide,
+    threejs,
+  } from "../assets";
+  
+  export const navLinks = [
+    {
+      id: "about",
+      title: "About",
+    },
+    {
+      id: "work",
+      title: "Work",
+    },
+    {
+      id: "contact",
+      title: "Contact",
+    },
+  ];
+  
+  export const services = [
+    {
+      title: "Web Developer",
+      icon: web,
+    },
+    {
+      title: "React Native Developer",
+      icon: mobile,
+    },
+    {
+      title: "Backend Developer",
+      icon: backend,
+    },
+    {
+      title: "Content Creator",
+      icon: creator,
+    },
+  ];
+  
+export const technologies = [
+    {
+      name: "HTML 5",
+      icon: html,
+    },
+    {
+      name: "CSS 3",
+      icon: css,
+    },
+    {
+      name: "JavaScript",
+      icon: javascript,
+    },
+    {
+      name: "TypeScript",
+      icon: typescript,
+    },
+    {
+      name: "React JS",
+      icon: reactjs,
+    },
+    {
+      name: "Redux Toolkit",
+      icon: redux,
+    },
+    {
+      name: "Tailwind CSS",
+      icon: tailwind,
+    },
+    {
+      name: "Node JS",
+      icon: nodejs,
+    },
+    {
+      name: "MongoDB",
+      icon: mongodb,
+    },
+    {
+      name: "Three JS",
+      icon: threejs,
+    },
+    {
+      name: "git",
+      icon: git,
+    },
+    {
+      name: "figma",
+      icon: figma,
+    },
+    {
+      name: "docker",
+      icon: docker,
+    },
+  ];
+  
+  export const experiences = [
+    {
+      title: "MCA | COMPUTER APPLICATIONS",
+      company_name: ["CHANDIGARH UNIVERSITY, GHARUAN, MOHALI"],
+      icon: starbucks,
+      iconBg: "#383E56",
+      date: "Aug 2021 - June 2023",
+      points: [
+       " As i am a Mca student i always busy to find the sollution of a real life problem." ,
+       "I have a In-depth knowledge in programming languages, software engineering, database management.",
+       "Skills in project management, teamwork, and problem-solving.",
+       "Familiarity with emerging technologies and their applications in various industries.",
+       "Ability to conduct research and analyze data to solve complex problems."
+      ],
+    },
+    {
+      title: "BSC | CHEMISTRY HONRS",
+      company_name: "Utkal University Vani Vihar,Bhubaneswar, Odisha",
+      icon: tesla,
+      iconBg: "#E6DEDD",
+      date: "Jan 2018 - Feb 2021",
+      points: [
+        "In addition to gaining subject-specific knowledge, studying chemistry,math,physics in the graduation can also help me develop a range of transferable skills, such as critical thinking, problem-solving, data analysis, and effective communication.",
+        "These skills are highly valued in many industries and can be applied to a wide range of professional roles."
+      ],
+    },
+    {
+      title: "Frontend Web Development",
+      company_name: "Learing from Ineuron",
+      icon: shopify,
+      iconBg: "#383E56",
+      date: "Jan 2022 - Till Now ",
+      points: [
+        "Developing and maintaining web applications using React.js and other related technologies(html,css,js).",
+        "Making so many projects on the basis of that knowledge",
+        "Ex-Gym Wbsites,Car rental system, Portfolio,Online Pglife system"
+      ],
+    },
+    {
+      title: "Full stack web Development",
+      company_name: "Learning From Ineuron",
+      icon: meta,
+      iconBg: "#E6DEDD",
+      date: "Jan 2023 - Present",
+      points: [
+        "By Learning full-stack development involves me gaining knowledge and skills in both front-end and back-end development",
+         "As well as understanding how to integrate them to build full-fledged web applications.",
+        "Making so many projects on the basis of that knowledge.",
+        "Ex-Car Rental System , Online Pg life system"
+      ],
+    },
+  ];
+  
+  export const projects = [
+    {
+      name: "Fast Car Rental System",
+      description:
+        "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "mongodb",
+          color: "green-text-gradient",
+        },
+        {
+          name: "tailwind",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: carrent,
+      source_code_link: "https://github.com/",
+    },
+    {
+      name: "Online PG Accomodation",
+      description:
+        "It is web application. It can used to locate places to rent and stay from all around the india using an online pg system. There will be users to utilise the pg system to find a home or apartment to rent, as well as people who do the opposite. In order to avoid any misunderstanding, we shall refer to users who locate and rent out residences as guests and people who own the houses. The ability to update, add, and delete residences allows hosts to negotiate with other visitors.",
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "PHP",
+          color: "green-text-gradient",
+        },
+        {
+          name: "css",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: jobit,
+      source_code_link: "https://github.com/",
+    },
+    {
+      name: "Trip Guide",
+      description:
+        "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+      tags: [
+        {
+          name: "nextjs",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "",
+          color: "green-text-gradient",
+        },
+        {
+          name: "css",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: tripguide,
+      source_code_link: "https://github.com/",
+    },
+  ];
+  
+  
